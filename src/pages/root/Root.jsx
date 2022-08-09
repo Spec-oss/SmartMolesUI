034b35df38b8fb0c6a182c799846e735422e7d4e
Akın Turan <akin.turan@outlook.com.tr>
2022-08-09 17:37:41 +0300

    root
    root

--- src/pages/root/Root.jsx
+++ src/pages/root/Root.jsx
@@ -3,17 +3,18 @@ import { Grid, Paper, Box, Typography, Card, CardContent, Badge } from '@mui/mat
 
 const Root = () => {
   return (
-    <Grid>
-      <Paper item xs={12} lg={6}
-        style={{
-          textAlign: 'center',
-          margin: '10px 10px',
-          padding: '10px 10px',
-          borderWidth: 11,
-        }}
-      >
-        <Grid item xs={12} lg={6}>
-          <Card sx={{ minWidth: 'auto', marginBottom: '20px' }}>
+    <Paper
+      style={{
+        textAlign: 'center',
+        margin: '10px 10px',
+        padding: '10px 10px',
+        borderWidth: 11,
+        background:"#e3ecf4"
+      }}
+    >
+      <Grid container>
+        <Grid item xs={12} md={6} style={{borderRightStyle:'inset',borderRightWidth:'thin', borderRightColor:'white'}}>
+          <Card sx={{  maxWidth: 'auto', margin: '0px 15px 15px 15px' }}>
             <CardContent>
               <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
                 Tarih : 2021-06-14 21:18:43, 2.Veri
@@ -23,8 +24,8 @@ const Root = () => {
           <Grid container style={{ margin: '10px' }}>
             <Grid justifyContent={'center'} display={'flex'} item xs={3}>
               <Box>
-              <Badge badgeContent={1} color="primary">
-                <img width={'85px'} src="https://i.ibb.co/kQLqnHg/m2i8-G6b1-G6b1-A0-N4.png" alt="Root" />
+                <Badge badgeContent={1} color="primary">
+                  <img width={'85px'} src="https://i.ibb.co/kQLqnHg/m2i8-G6b1-G6b1-A0-N4.png" alt="Root" />
                 </Badge>
                 <Typography variant="subtitle2" gutterBottom component="div">
                   10
@@ -281,8 +282,277 @@ const Root = () => {
             </Grid>
           </Grid>
         </Grid>
-      </Paper>
-    </Grid>
+        <Grid item xs={12} md={6}>
+          <Card sx={{ maxWidth: 'auto', margin: '0px 15px 15px 15px' }}>
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                Tarih : 2021-06-14 21:18:43, 2.Veri
+              </Typography>
+            </CardContent>
+          </Card>
+          <Grid container style={{ margin: '10px' }}>
+            <Grid justifyContent={'center'} display={'flex'} item xs={3}>
+              <Box>
+                <Badge badgeContent={1} color="primary">
+                  <img width={'85px'} src="https://i.ibb.co/kQLqnHg/m2i8-G6b1-G6b1-A0-N4.png" alt="Root" />
+                </Badge>
+                <Typography variant="subtitle2" gutterBottom component="div">
+                  10
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid justifyContent={'center'} display={'flex'} item xs={3}>
+              <Box>
+                <img width={'85px'} src="https://i.ibb.co/kQLqnHg/m2i8-G6b1-G6b1-A0-N4.png" alt="Root" />
+                <Typography variant="subtitle2" gutterBottom component="div">
+                  10
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid justifyContent={'center'} display={'flex'} item xs={3}>
+              <Box>
+                <img width={'85px'} src="https://i.ibb.co/kQLqnHg/m2i8-G6b1-G6b1-A0-N4.png" alt="Root" />
+                <Typography variant="subtitle2" gutterBottom component="div">
+                  10
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid justifyContent={'center'} display={'flex'} item xs={3}>
+              <Box>
+                <img width={'85px'} src="https://i.ibb.co/kQLqnHg/m2i8-G6b1-G6b1-A0-N4.png" alt="Root" />
+                <Typography variant="subtitle2" gutterBottom component="div">
+                  10
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid justifyContent={'center'} display={'flex'} item xs={3}>
+              <Box>
+                <img width={'85px'} src="https://i.ibb.co/kQLqnHg/m2i8-G6b1-G6b1-A0-N4.png" alt="Root" />
+                <Typography variant="subtitle2" gutterBottom component="div">
+                  10
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid justifyContent={'center'} display={'flex'} item xs={3}>
+              <Box>
+                <img width={'85px'} src="https://i.ibb.co/kQLqnHg/m2i8-G6b1-G6b1-A0-N4.png" alt="Root" />
+                <Typography variant="subtitle2" gutterBottom component="div">
+                  10
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid justifyContent={'center'} display={'flex'} item xs={3}>
+              <Box>
+                <img width={'85px'} src="https://i.ibb.co/kQLqnHg/m2i8-G6b1-G6b1-A0-N4.png" alt="Root" />
+                <Typography variant="subtitle2" gutterBottom component="div">
+                  10
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid justifyContent={'center'} display={'flex'} item xs={3}>
+              <Box>
+                <img width={'85px'} src="https://i.ibb.co/kQLqnHg/m2i8-G6b1-G6b1-A0-N4.png" alt="Root" />
+                <Typography variant="subtitle2" gutterBottom component="div">
+                  10
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid justifyContent={'center'} display={'flex'} item xs={3}>
+              <Box>
+                <img width={'85px'} src="https://i.ibb.co/kQLqnHg/m2i8-G6b1-G6b1-A0-N4.png" alt="Root" />
+                <Typography variant="subtitle2" gutterBottom component="div">
+                  10
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid justifyContent={'center'} display={'flex'} item xs={3}>
+              <Box>
+                <img width={'85px'} src="https://i.ibb.co/kQLqnHg/m2i8-G6b1-G6b1-A0-N4.png" alt="Root" />
+                <Typography variant="subtitle2" gutterBottom component="div">
+                  10
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid justifyContent={'center'} display={'flex'} item xs={3}>
+              <Box>
+                <img width={'85px'} src="https://i.ibb.co/kQLqnHg/m2i8-G6b1-G6b1-A0-N4.png" alt="Root" />
+                <Typography variant="subtitle2" gutterBottom component="div">
+                  10
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid justifyContent={'center'} display={'flex'} item xs={3}>
+              <Box>
+                <img width={'85px'} src="https://i.ibb.co/kQLqnHg/m2i8-G6b1-G6b1-A0-N4.png" alt="Root" />
+                <Typography variant="subtitle2" gutterBottom component="div">
+                  10
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid justifyContent={'center'} display={'flex'} item xs={3}>
+              <Box>
+                <img width={'85px'} src="https://i.ibb.co/kQLqnHg/m2i8-G6b1-G6b1-A0-N4.png" alt="Root" />
+                <Typography variant="subtitle2" gutterBottom component="div">
+                  10
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid justifyContent={'center'} display={'flex'} item xs={3}>
+              <Box>
+                <img width={'85px'} src="https://i.ibb.co/kQLqnHg/m2i8-G6b1-G6b1-A0-N4.png" alt="Root" />
+                <Typography variant="subtitle2" gutterBottom component="div">
+                  10
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid justifyContent={'center'} display={'flex'} item xs={3}>
+              <Box>
+                <img width={'85px'} src="https://i.ibb.co/kQLqnHg/m2i8-G6b1-G6b1-A0-N4.png" alt="Root" />
+                <Typography variant="subtitle2" gutterBottom component="div">
+                  10
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid justifyContent={'center'} display={'flex'} item xs={3}>
+              <Box>
+                <img width={'85px'} src="https://i.ibb.co/kQLqnHg/m2i8-G6b1-G6b1-A0-N4.png" alt="Root" />
+                <Typography variant="subtitle2" gutterBottom component="div">
+                  10
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid justifyContent={'center'} display={'flex'} item xs={3}>
+              <Box>
+                <img width={'85px'} src="https://i.ibb.co/kQLqnHg/m2i8-G6b1-G6b1-A0-N4.png" alt="Root" />
+                <Typography variant="subtitle2" gutterBottom component="div">
+                  10
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid justifyContent={'center'} display={'flex'} item xs={3}>
+              <Box>
+                <img width={'85px'} src="https://i.ibb.co/kQLqnHg/m2i8-G6b1-G6b1-A0-N4.png" alt="Root" />
+                <Typography variant="subtitle2" gutterBottom component="div">
+                  10
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid justifyContent={'center'} display={'flex'} item xs={3}>
+              <Box>
+                <img width={'85px'} src="https://i.ibb.co/kQLqnHg/m2i8-G6b1-G6b1-A0-N4.png" alt="Root" />
+                <Typography variant="subtitle2" gutterBottom component="div">
+                  10
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid justifyContent={'center'} display={'flex'} item xs={3}>
+              <Box>
+                <img width={'85px'} src="https://i.ibb.co/kQLqnHg/m2i8-G6b1-G6b1-A0-N4.png" alt="Root" />
+                <Typography variant="subtitle2" gutterBottom component="div">
+                  10
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid justifyContent={'center'} display={'flex'} item xs={3}>
+              <Box>
+                <img width={'85px'} src="https://i.ibb.co/kQLqnHg/m2i8-G6b1-G6b1-A0-N4.png" alt="Root" />
+                <Typography variant="subtitle2" gutterBottom component="div">
+                  10
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid justifyContent={'center'} display={'flex'} item xs={3}>
+              <Box>
+                <img width={'85px'} src="https://i.ibb.co/kQLqnHg/m2i8-G6b1-G6b1-A0-N4.png" alt="Root" />
+                <Typography variant="subtitle2" gutterBottom component="div">
+                  10
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid justifyContent={'center'} display={'flex'} item xs={3}>
+              <Box>
+                <img width={'85px'} src="https://i.ibb.co/kQLqnHg/m2i8-G6b1-G6b1-A0-N4.png" alt="Root" />
+                <Typography variant="subtitle2" gutterBottom component="div">
+                  10
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid justifyContent={'center'} display={'flex'} item xs={3}>
+              <Box>
+                <img width={'85px'} src="https://i.ibb.co/kQLqnHg/m2i8-G6b1-G6b1-A0-N4.png" alt="Root" />
+                <Typography variant="subtitle2" gutterBottom component="div">
+                  10
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid justifyContent={'center'} display={'flex'} item xs={3}>
+              <Box>
+                <img width={'85px'} src="https://i.ibb.co/kQLqnHg/m2i8-G6b1-G6b1-A0-N4.png" alt="Root" />
+                <Typography variant="subtitle2" gutterBottom component="div">
+                  10
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid justifyContent={'center'} display={'flex'} item xs={3}>
+              <Box>
+                <img width={'85px'} src="https://i.ibb.co/kQLqnHg/m2i8-G6b1-G6b1-A0-N4.png" alt="Root" />
+                <Typography variant="subtitle2" gutterBottom component="div">
+                  10
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid justifyContent={'center'} display={'flex'} item xs={3}>
+              <Box>
+                <img width={'85px'} src="https://i.ibb.co/kQLqnHg/m2i8-G6b1-G6b1-A0-N4.png" alt="Root" />
+                <Typography variant="subtitle2" gutterBottom component="div">
+                  10
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid justifyContent={'center'} display={'flex'} item xs={3}>
+              <Box>
+                <img width={'85px'} src="https://i.ibb.co/kQLqnHg/m2i8-G6b1-G6b1-A0-N4.png" alt="Root" />
+                <Typography variant="subtitle2" gutterBottom component="div">
+                  10
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid justifyContent={'center'} display={'flex'} item xs={3}>
+              <Box>
+                <img width={'85px'} src="https://i.ibb.co/kQLqnHg/m2i8-G6b1-G6b1-A0-N4.png" alt="Root" />
+                <Typography variant="subtitle2" gutterBottom component="div">
+                  10
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid justifyContent={'center'} display={'flex'} item xs={3}>
+              <Box>
+                <img width={'85px'} src="https://i.ibb.co/kQLqnHg/m2i8-G6b1-G6b1-A0-N4.png" alt="Root" />
+                <Typography variant="subtitle2" gutterBottom component="div">
+                  10
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid justifyContent={'center'} display={'flex'} item xs={3}>
+              <Box>
+                <img width={'85px'} src="https://i.ibb.co/kQLqnHg/m2i8-G6b1-G6b1-A0-N4.png" alt="Root" />
+                <Typography variant="subtitle2" gutterBottom component="div">
+                  10
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid justifyContent={'center'} display={'flex'} item xs={3}>
+              <Box>
+                <img width={'85px'} src="https://i.ibb.co/kQLqnHg/m2i8-G6b1-G6b1-A0-N4.png" alt="Root" />
+                <Typography variant="subtitle2" gutterBottom component="div">
+                  10
+                </Typography>
+              </Box>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Paper>
   );
 };
 
